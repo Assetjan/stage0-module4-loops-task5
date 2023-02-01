a318927d0c3e4a9a24777add38ccbaade79520f7
Asset <assetziyabek@gmail.com>
2023-02-01 10:04:03 +0600

    done

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,21 +2,19 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-        int k = 1;
-        int l = 0;
-        int h;
         for (int i = 0; i < sideLength; i++){
             for (int j = 0; j<sideLength; j++){
-                h = j+1;
-                if (k == j+1 || h == sideLength - l){
-                    System.out.print('8');
+                if (i != sideLength/2) {
+                    if (j == sideLength/2) {
+                        System.out.print('8');
+                    }else{
+                        System.out.print(' ');
+                    }
                 }else{
-                    System.out.print(' ');
+                    System.out.print('8');
                 }
             }
             System.out.println("");
-            k++;
-            l++;
         }
     }
 }
