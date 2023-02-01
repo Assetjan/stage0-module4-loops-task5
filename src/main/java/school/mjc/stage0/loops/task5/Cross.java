e906ad5cd4322a4deb0f0d626c21b693d9ca32a1
Asset <assetziyabek@gmail.com>
2023-02-01 09:43:28 +0600

    done

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,22 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-
+        int k = 1;
+        int l = 0;
+        int h;
+        for (int i = 0; i < sideLength; i++){
+            for (int j = 0; j<sideLength; j++){
+                h = j+1;
+                if (k == j+1 || h == sideLength - l){
+                    System.out.print('8');
+                }else{
+                    System.out.print(' ');
+                }
+            }
+            System.out.println("");
+            k++;
+            l++;
+        }
     }
 }
+
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,40 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        for (int i = 0; i < height; i++){
 
+            for(int j = 1; j <= height; j++){
+                if (height%2 != 0){
+                    if(i<=height/2){
+                        if (j > i && j <= height - i){
+                            System.out.print('8');
+                        }else{
+                            System.out.print(' ');
+                        }
+                    }else{
+                        if(j <= i+1 && j >= height - i){
+                            System.out.print('8');
+                        }else{
+                            System.out.print(' ');
+                        }
+                    }
+                }else{
+                    if(i<height/2){
+                        if (j > i && j <= height - i){
+                            System.out.print('8');
+                        }else{
+                            System.out.print(' ');
+                        }
+                    }else{
+                        if(j <= i+1 && j >= height - i){
+                            System.out.print('8');
+                        }else{
+                            System.out.print(' ');
+                        }
+                    }
+                }
+            }
+            System.out.println("");
+        }
     }
 }
